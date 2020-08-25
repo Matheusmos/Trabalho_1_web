f7774311b327135400de52031fc249e01001e642
Matheus Oliveira <matheus.engsoftware@gmail.com>
2020-08-24 23:07:38 -0300

    organização de arquivos
    separação mais nítida entre html, css e js

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-/*const btn = document.querySelectorAll(".botao")
-const fundo = document.querySelectorAll(".fundo")
-for(let i=0; i < btn.length; i++){
-    btn[i].addEventListener("click", function(){
-        fundo.style.background = this.getAttribute("data-color")
-        this.style.color = this.getAttribute("data-color")
-    })
-}*/
-
 var color = ["#030212","#1c08e2","#211A4F"]
 
-document.querySelector("button#botao1").addEventListener("click",
+    document.querySelector("button#botao1").addEventListener("click",
+    function(){
+        document.querySelector("body").style.background = color[0]
+    });
+
+    document.querySelector("button#botao2").addEventListener("click",
     function(){
         document.querySelector("body").style.background = color[1]
+    });
+
+    document.querySelector("button#botao3").addEventListener("click",
+    function(){
+        document.querySelector("body").style.background = color[2]
     });
