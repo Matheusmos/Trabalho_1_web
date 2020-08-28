fd014b7d0815efc8080b92108e47a19b7c2e08f2
tamiresariane <eng.soft.tamires@gmail.com>
2020-08-28 13:19:54 -0300

    envio de mensagens e mural

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var color = ["#030212","#1c08e2","#211A4F"]
-
+var color = ["#30336b","#4834d4","#686de0"]
+var contador = 0;
     document.querySelector("button#botao1").addEventListener("click",
     function(){
         document.querySelector("body").style.background = color[0]
@@ -14,3 +14,30 @@ var color = ["#030212","#1c08e2","#211A4F"]
     function(){
         document.querySelector("body").style.background = color[2]
     });
+
+
+    function enviar () { 
+        var mensagem = document.createElement("div");
+        mensagem.setAttribute('id', contador);
+        document.getElementById("mural").appendChild(mensagem);
+        var nome = document.getElementById("nm").value; 
+        var msg = document.getElementById("mgm").value;
+        var conteudo = document.createTextNode(nome + ": " + msg);
+        var botao = document.createElement("button");
+        botao.setAttribute('id', contador);
+        botao.setAttribute('onClick', "excluir(this.id)");
+        var nomeBotao= document.createTextNode("Excluir");
+        botao.appendChild(nomeBotao);
+        
+        mensagem.appendChild(conteudo);
+        mensagem.appendChild(botao);
+        
+        contador++;
+        
+      }
+      
+      function excluir(id){
+      	document.getElementById(id).remove();
+      }
+      
+      
